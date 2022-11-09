53a4d39c5c6069d077dfb7b397535ba61f1de696
Maritim Daniel <maritimdaniel07@gmai.com>
2022-11-09 11:46:13 +0300

    add hero section

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Box, styled, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import heroImg from "../media/hero_illustration.png";
+
+const Hero = () => {
+  const CustomBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "center",
+    gap: theme.spacing(5),
+    marginTop: theme.spacing(3),
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+      textAlign: "center",
+    },
+  }));
+
+  const Title = styled(Typography)(({ theme }) => ({
+    fontSize: "64px",
+    color: "#000336",
+    fontWeight: "bold",
+    margin: theme.spacing(4, 0, 4, 0),
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "40px",
+    },
+  }));
+
+  return (
+    <Box sx={{ backgroundColor: "#E6F0FF", minHeight: "80vh" }}>
+      <Container>
+        <CustomBox>
+          <Box sx={{ flex: "1" }}>
+            <Typography
+              variant="body2"
+              sx={{
+                fontSize: "18px",
+                color: "#687690",
+                fontWeight: "500",
+                mt: 10,
+                mb: 4,
+              }}
+            >
+              Welcome to Realter Agency
+            </Typography>
+            <Title variant="h1">Find the Best Living Experience.</Title>
+            <Typography
+              variant="body2"
+              sx={{ fontSize: "18px", color: "#5A6473", my: 4 }}
+            >
+              Make your life more comfortable by living in a house that suits
+              your style.
+            </Typography>
+          </Box>
+
+          <Box sx={{ flex: "1.25" }}>
+            <img
+              src={heroImg}
+              alt="heroImg"
+              style={{ maxWidth: "100%", marginBottom: "2rem" }}
+            />
+          </Box>
+        </CustomBox>
+      </Container>
+    </Box>
+  );
+};
+
+export default Hero;
