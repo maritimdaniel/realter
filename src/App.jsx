98851c0aa68470f9a516573d9db1284169ba1809
Maritim Daniel <maritimdaniel07@gmai.com>
2022-11-25 09:54:57 +0300

    add companies section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Components/Hero";
+import Companies from "./Components/Companies";
 
 function App() {
   return (
     <>
       <Hero />
+      <Companies />
     </>
   );
 }
