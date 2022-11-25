e61b0c5867b623e0d03e70105775bdb65bbaebc9
Maritim Daniel <maritimdaniel07@gmai.com>
2022-11-25 09:59:51 +0300

    add guide section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Components/Hero";
 import Companies from "./Components/Companies";
+import Guide from "./Components/Guide";
 
 function App() {
   return (
     <>
       <Hero />
       <Companies />
+      <Guide />
     </>
   );
 }
